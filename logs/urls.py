fedf5f7ec948b338c9944b690a93f6fa919cc3fb
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-07-30 17:21:39 +0900

    ROUT-4 logアプリケーションを追加する

---
+++ logs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.log_list, name='log_list'),
+    path('<int:pk>/', views.log_detail, name='log_detail'),
+    path('new', views.log_new, name='log_new'),
+    path('<int:pk>/edit/', views.log_edit, name='log_edit'),
+    path('<int:pk>/delete/', views.log_delete, name='log_delete'),
+]
