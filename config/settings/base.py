8b6d3e104275397ff331dd591a4d0cf323d8a61f
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-08-13 11:57:05 +0900

    ROUT-13 設定ファイルを修正する

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# Build paths inside the project like this: DEFAULT_BASE_DIR / 'subdir'.
+DEFAULT_BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Application definition
-INSTALLED_APPS = [
+DEFAULT_INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     "logs"
 ]
 
-MIDDLEWARE = [
+DEFAULT_MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -38,12 +38,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "config.urls"
+DEFAULT_ROOT_URLCONF = "config.urls"
 
-TEMPLATES = [
+DEFAULT_TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(DEFAULT_BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -56,7 +56,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+DEFAULT_WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
@@ -65,14 +65,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 DEFAULT_DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": DEFAULT_BASE_DIR / "db.sqlite3",
     }
 }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+DEFAULT_AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
@@ -91,31 +91,31 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "ja"
+DEFAULT_LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "Asia/Tokyo"
+DEFAULT_TIME_ZONE = "Asia/Tokyo"
 
-USE_I18N = True
+DEFAULT_USE_I18N = True
 
-USE_TZ = True
+DEFAULT_USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRES = [BASE_DIR / "static"]
-STATIC_ROOT = f"/var/www/{BASE_DIR.name}/static"
+DEFAULT_STATIC_URL = "static/"
+DEFAULT_STATICFILES_DIRES = [DEFAULT_BASE_DIR / "static"]
+DEFAULT_STATIC_ROOT = f"/var/www/{DEFAULT_BASE_DIR.name}/static"
 
 # Media files
-MEDIA_URL = "/media/"
-MEDIA_ROOT = f"/var/www/{BASE_DIR.name}/media"
+DEFAULT_MEDIA_URL = "/media/"
+DEFAULT_MEDIA_ROOT = f"/var/www/{DEFAULT_BASE_DIR.name}/media"
 
 # Test in local environment
-MEDIA_ROOT =  BASE_DIR / "media_root"
+DEFAULT_MEDIA_ROOT =  DEFAULT_BASE_DIR / "media_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
