51f151ca66ded9013b71c1dc6828e78d4e649590
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-09-02 18:27:33 +0900

    nsk-34 URLの文言を修正する

--- snake/urls.py
+++ snake/urls.py
@@ -3,6 +3,6 @@ from .views import emotion
 
 urlpatterns = [
     path('', emotion.emotion_list, name='emotion_list'),
-    path('new', emotion.log_new, name='emotion_new'),
-    path('<int:pk>/delete/', emotion.log_delete, name='emotion_delete'),
+    path('new', emotion.emotion_new, name='emotion_new'),
+    path('<int:pk>/delete/', emotion.emotion_delete, name='emotion_delete'),
 ]
