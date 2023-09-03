031b001d7d71eef68b770569605f947cf32d1460
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-09-03 17:55:08 +0900

    nsk-57 URLの設定をする

--- coala/urls.py
+++ coala/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import record, performance
+from .challenge import record, performance, challenge
 
 urlpatterns = [
     # Recordを管理するAPI
@@ -13,4 +13,11 @@ urlpatterns = [
     path('performance/new', performance.performance_new, name='performance_new'),
     path('performance/<int:pk>/edit/', performance.performance_edit, name='performance_edit'),
     path('performance/<int:pk>/delete/', performance.performance_delete, name='performance_delete'),
+    # Challengeを管理するAPI
+    path('challenge/list', challenge.challenge_list, name='challenge_list'),
+    path('challenge/<int:pk>/', challenge.challenge_detail, name='challenge_detail'),
+    path('challenge/new', challenge.challenge_new, name='challenge_new'),
+    path('challenge/<int:pk>/edit/', challenge.challenge_edit, name='challenge_edit'),
+    path('challenge/evaluation/<int:pk>/edit/', challenge.evaluation_edit, name='evaluation_edit'),
+    path('challenge/<int:pk>/delete/', challenge.challenge_delete, name='challenge_delete')
 ]
