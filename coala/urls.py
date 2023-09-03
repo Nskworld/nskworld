5eddcb3fa039f14ebf9e6750aee51bdfb908a7ab
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-09-03 16:14:02 +0900

    nsk-51 URL設定を追加する

--- coala/urls.py
+++ coala/urls.py
@@ -3,11 +3,15 @@ from .views import record
 
 urlpatterns = [
     # Recordを管理するAPI
-    path('record_list', record.record_list, name='record_list'),
-    path('<int:pk>/', record.record_detail, name='record_detail'),
-    path('new', record.record_new, name='record_new'),
-    path('<int:pk>/edit/', record.record_edit, name='record_edit'),
-    path('<int:pk>/delete/', record.record_delete, name='record_delete'),
+    path('record/list', record.record_list, name='record_list'),
+    path('record/<int:pk>/', record.record_detail, name='record_detail'),
+    path('record/new', record.record_new, name='record_new'),
+    path('record/<int:pk>/edit/', record.record_edit, name='record_edit'),
+    path('record/<int:pk>/delete/', record.record_delete, name='record_delete'),
     # Performanceを管理するAPI
-    
+    path('performance/list', record.record_list, name='performance_list'),
+    path('performance/<int:pk>/', record.record_detail, name='performance_detail'),
+    path('performance/new', record.record_new, name='performance_new'),
+    path('performance/<int:pk>/edit/', record.record_edit, name='performance_edit'),
+    path('<performance/int:pk>/delete/', record.record_delete, name='performance_delete'),
 ]
