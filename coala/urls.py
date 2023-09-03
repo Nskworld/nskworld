56e33678bf368c9e8763c7a25e66e086354e52bb
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-09-03 16:05:13 +0900

    nsk-51 URL設定を追加する

--- coala/urls.py
+++ coala/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from .views import record
 
 urlpatterns = [
-    path('', record.record_list, name='record_list'),
+    # Recordを管理するAPI
+    path('record_list', record.record_list, name='record_list'),
     path('<int:pk>/', record.record_detail, name='record_detail'),
     path('new', record.record_new, name='record_new'),
     path('<int:pk>/edit/', record.record_edit, name='record_edit'),
     path('<int:pk>/delete/', record.record_delete, name='record_delete'),
+    # Performanceを管理するAPI
+    
 ]
