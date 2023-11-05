8b015b53f57ce8e2b67c9ec116a039902c840419
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2023-11-05 23:28:27 +0900

    nsk-83 一括登録処理が完了した時にSlackへ結果を通知する処理を追加

--- bunny/views.py
+++ bunny/views.py
@@ -1,25 +1,34 @@
 # views.py
-from django.shortcuts import render
+import os
+from django.conf import settings
+from django.shortcuts import render 
+from .alert import SendMessageToSlack
 from .forms import UploadCSVForm
+from .worker import BulkRegistrationPerformance
 
 def upload_csv(request):
     """ 一括処理用のCSVファイルをアップロードする """
     message = ''
+    webhook_url = settings.WEBHOOK_URL
 
-    if request.method == "POST":
+    if request.method == 'POST':
+        file_path = f'media/upload_cache/{request.FILES["csv_file"].name}'
         form = UploadCSVForm(request.POST, request.FILES)
         if form.is_valid():
             try:
-                with open(f'media/upload_cache/{request.FILES["csv_file"].name}', 'wb+') as destination:
+                with open(file_path, 'wb+') as destination:
                     for chunk in request.FILES["csv_file"].chunks():
-                        destination.write(chunk)
-                message = "ファイルアップロードに成功しました"
+                        destination.write(chunk)  
+                worker = BulkRegistrationPerformance()
+                worker.process_csv_in_threads(file_path)
+                message = 'ファイルアップロードに成功しました'
+                os.remove(file_path)
             except Exception as e:
-                message = "ファイルアップロードに失敗しました: " + str(e)
+                message = 'ファイルアップロードに失敗しました: ' + str(e)
         else:
-            message = "ファイルアップロードに失敗しました"
-
+            message = 'ファイルアップロードに失敗しました'
     else:
         form = UploadCSVForm()
 
+    SendMessageToSlack(webhook_url, message)
     return render(request, 'bunny/upload.html', {'form': form, 'message': message})
